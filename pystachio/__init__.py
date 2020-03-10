d38bcbbc5bd1c3483eb97003587033903b886f7a
tomtwttr <33262427+tomtwttr@users.noreply.github.com>
2020-03-10 14:42:06 -0700

    Choice with container equality & CI fixes (#38)
    * Choice with container equality
    
    * Fix isort
    
    * Trusty dist for python 2.6
    
    * Isort check dropped 2.7 in January

--- pystachio/__init__.py
+++ pystachio/__init__.py
@@ -4,31 +4,15 @@ __license__ = 'MIT'
 
 
 import sys
-if sys.version_info < (2, 6, 5):
-  raise ImportError("pystachio requires Python >= 2.6.5")
-
 
 from .base import Environment
-from .basic import (
-    Boolean,
-    Enum,
-    Float,
-    Integer,
-    String)
-from .choice import (
-   Choice
-)
-from .composite import (
-    Default,
-    Empty,
-    Required,
-    Struct)
-from .container import (
-    List,
-    Map)
+from .basic import Boolean, Enum, Float, Integer, String
+from .choice import Choice
+from .composite import Default, Empty, Required, Struct
+from .container import List, Map
 from .naming import Namable, Ref
 from .parsing import MustacheParser
-from .typing import (
-    Type,
-    TypeCheck,
-    TypeFactory)
+from .typing import Type, TypeCheck, TypeFactory
+
+if sys.version_info < (2, 6, 5):
+  raise ImportError("pystachio requires Python >= 2.6.5")
